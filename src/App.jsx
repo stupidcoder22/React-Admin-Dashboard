054a3aa5a3cbbc127fe99cc2ab5eba08ed5863c9
prateek <stupidcoder22@gmail.com>
2022-05-09 09:28:38 +0530

    topbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Topbar from "./Components/Topbar/Topbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Topbar />
+    </div>
+  );
 }
 
 export default App;
