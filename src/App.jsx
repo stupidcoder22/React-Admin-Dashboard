28cb34f55ed94df257bedf9cb2ed9f36216f8e35
prateek <stupidcoder22@gmail.com>
2022-05-09 09:45:56 +0530

    Sidebar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Sidebar from "./Components/Sidebar/Sidebar";
 import Topbar from "./Components/Topbar/Topbar";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <div className="container">
+        <Sidebar />
+      </div>
     </div>
   );
 }
