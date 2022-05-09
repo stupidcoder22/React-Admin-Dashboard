9f0aae44af4d685b434a61a7c80df582f4c4f503
prateek <stupidcoder22@gmail.com>
2022-05-09 13:58:22 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,33 @@ import "./App.css";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Topbar from "./Components/Topbar/Topbar";
 import Home from "./pages/Home/Home";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import UserList from "./pages/Userlist/Userlist";
+import User from "./pages/User/User";
+import NewUser from "./pages/Newuser/Newuser";
+import Product from "./pages/Product/Product";
+import NewProduct from "./pages/Newproduct/Newproduct";
+import ProductList from "./pages/Productlist/Productlist";
 
 function App() {
   return (
-    <div className="App">
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Topbar />
+        <div className="container">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="user" element={<UserList />} />
+            <Route path="user/:id" element={<User />} />
+            <Route path="newuser" element={<NewUser />} />
+            <Route path="products" element={<ProductList />} />
+            <Route path="product/:id" element={<Product />} />
+            <Route path="newproduct" element={<NewProduct />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -13,7 +13,7 @@ import {
   WorkOutline,
   Report,
 } from "@material-ui/icons";
-
+import { Link } from "react-router-dom";
 export default function Sidebar() {
   return (
     <div className="sidebar">
@@ -21,10 +21,12 @@ export default function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem active">
-              <LineStyle className="sidebarIcon" />
-              Home
-            </li>
+            <Link to="/" className="link">
+              <li className="sidebarListItem active">
+                <LineStyle className="sidebarIcon" />
+                Home
+              </li>
+            </Link>
             <li className="sidebarListItem">
               <Timeline className="sidebarIcon" />
               Analytics
@@ -38,14 +40,18 @@ export default function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Quick Menu</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <PermIdentity className="sidebarIcon" />
-              Users
-            </li>
-            <li className="sidebarListItem">
-              <Storefront className="sidebarIcon" />
-              Products
-            </li>
+            <Link to="/users" className="link">
+              <li className="sidebarListItem">
+                <PermIdentity className="sidebarIcon" />
+                Users
+              </li>
+            </Link>
+            <Link to="/products" className="link">
+              <li className="sidebarListItem">
+                <Storefront className="sidebarIcon" />
+                Products
+              </li>
+            </Link>
             <li className="sidebarListItem">
               <AttachMoney className="sidebarIcon" />
               Transactions
